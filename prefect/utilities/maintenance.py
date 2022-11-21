534da11c5387a1179863eaddf73e2cdcfdaa6a19
Patrick O'Connor <patrickoconnor80@Patricks-MacBook-Pro-2.local>
2022-11-21 15:14:13 -0500

    j

--- prefect/utilities/maintenance.py
+++ prefect/utilities/maintenance.py
@@ -2,19 +2,6 @@ import prefect
 from prefect import flow, get_run_logger
 from platform import node, platform, python_version
 from prefect.orion.api.server import ORION_API_VERSION as API
-from prefect.blocks.core import Block
-from prefect.orion.schemas.schedules import CronSchedule
-from maintenance import maintenance
-
-storage = Block.load("s3/prod")
-
-deployment = Deployment.build_from_flow(
-    flow=maintenance,
-    name="maintenance-deployment",
-    work_queue_name="mlops",
-    storage=storage,
-    schedule=(CronSchedule(cron="0 0 * * *", timezone="America/New_York"))
-)
 
 @flow
 def maintenance():
@@ -24,7 +11,5 @@ def maintenance():
     logger.info("Python = %s. API: %s. Prefect = %s 🚀", python_version(), API, version)
 
 
-# if __name__ == "__main__":
-#     maintenance()
-
-deployment.apply()
+if __name__ == "__main__":
+    maintenance()
