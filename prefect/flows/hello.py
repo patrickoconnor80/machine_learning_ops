2dd7b3754812f06e78d7c063171b867368593bc1
Patrick O'Connor <patrickoconnor80@Patricks-MacBook-Pro-2.local>
2022-11-21 18:16:28 -0500

    u

--- prefect/flows/hello.py
+++ prefect/flows/hello.py
@@ -34,6 +34,6 @@ deployment = Deployment.build_from_flow(
 
 if __name__ == "__main__":
     deployment.apply()
-    
 
-from prefect.libs.snowflake_client import SnowflakeClient
+
+from libs.snowflake_client import SnowflakeClient
